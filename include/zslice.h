1553e9cccea30911dbf2adbef3700277f5431e5b
Helmut Lord <kellyhlord@gmail.com>
2025-06-12 17:03:10 -0400

    Added hookup to serial peripheral

--- include/zslice.h
+++ include/zslice.h
@@ -8,6 +8,8 @@
 #include <zephyr/linker/sections.h>
 #include <zephyr/toolchain.h>
 #include <zephyr/sys/util.h>
+#include <zephyr/drivers/uart.h>
+#include <zephyr/net_buf.h>
 
 #include <pb.h>
 
@@ -20,39 +22,61 @@ typedef void (*zslice_handler_cb_t)(const void *req, void *resp, void *user_data
 
 // Internal struct defining a single packet handler.
 struct zslice_handler {
-    uint16_t packet_index;
-    const pb_msgdesc_t *request_msg;
-    const pb_msgdesc_t *response_msg;
-    zslice_handler_cb_t callback;
-    uint32_t flags;
+	uint16_t packet_index;
+	const pb_msgdesc_t *request_msg;
+	const pb_msgdesc_t *response_msg;
+	zslice_handler_cb_t callback;
+	uint32_t flags;
 };
 
 // Internal struct defining a zslice communication service.
 struct zslice_service {
-    const struct device *dev;
-    const struct zslice_handler *handlers;
-    size_t num_handlers;
-    void *user_data;
+	const struct device *dev;
+	const struct zslice_handler *handlers;
+	size_t num_handlers;
+	struct net_buf_pool *buf_pool;
+	void *user_data;
 };
 
-#define ZSLICE_HANDLER(_index, _req_type, _resp_type, _cb, _flags, ...) \
-    {                                                                   \
-        .packet_index = (_index),                                       \
-        .request_msg = (_req_type),                                     \
-        .response_msg = (_resp_type),                                   \
-        .callback = (zslice_handler_cb_t)(_cb),                         \
-        .flags = (_flags),                                              \
-    }
+int zslice_enable_rx(const struct device *dev);
+int zslice_disable_rx(const struct device *dev);
 
-#define ZSLICE_SERVICE_DEFINE(_name, _dt_node_id, _user_data, ...)       \
-    static const struct zslice_handler _name##_handlers[] = {            \
-        __VA_ARGS__                                                      \
-    };                                                                   \
-    const STRUCT_SECTION_ITERABLE(zslice_service, _name) = {             \
-        .dev = DEVICE_DT_GET(_dt_node_id),                               \
-        .handlers = _name##_handlers,                                    \
-        .num_handlers = ARRAY_SIZE(_name##_handlers),                    \
-        .user_data = _user_data,                                         \
-    };
+int zslice_interrupt_handler(const struct device *dev, struct net_buf_pool *buf_pool);
+
+#define ZSLICE_HANDLER(_index, _req_type, _resp_type, _cb, _flags, ...)                            \
+	{                                                                                              \
+		.packet_index = (_index),                                                                  \
+		.request_msg = (_req_type),                                                                \
+		.response_msg = (_resp_type),                                                              \
+		.callback = (zslice_handler_cb_t)(_cb),                                                    \
+		.flags = (_flags),                                                                         \
+	}
+
+#define ZSLICE_SERVICE_DEFINE(_name, _dt_node_id, _svc_bufs, _svc_buf_size, _user_data, ...)       \
+	static void _name##_interrupt_handler(const struct device *dev, void *user_data)               \
+	{                                                                                              \
+		struct net_buf_pool *buf_pool = (struct net_buf_pool *)(user_data);                        \
+		zslice_interrupt_handler(dev, buf_pool);                                                   \
+	}                                                                                              \
+	static const struct zslice_handler _name##_handlers[] = {__VA_ARGS__};                         \
+	NET_BUF_POOL_DEFINE(_name##_pool, (ARRAY_SIZE(_name##_handlers) * _svc_bufs),                  \
+			    _svc_buf_size, 0, NULL);                                                           \
+	STRUCT_SECTION_ITERABLE(zslice_service, _name) = {                                             \
+		.dev = DEVICE_DT_GET(_dt_node_id),                                                         \
+		.handlers = _name##_handlers,                                                              \
+		.num_handlers = ARRAY_SIZE(_name##_handlers),                                              \
+		.buf_pool = &(_name##_pool),                                                               \
+		.user_data = _user_data,                                                                   \
+	};                                                                                             \
+	static int _name##_init(void)                                                                  \
+	{                                                                                              \
+		if (!device_is_ready(DEVICE_DT_GET(_dt_node_id))) {                                        \
+			return -ENODEV;                                                                        \
+		}                                                                                          \
+		return uart_irq_callback_user_data_set(DEVICE_DT_GET(_dt_node_id),                         \
+						       _name##_interrupt_handler,                                          \
+						       (void *)&(_name##_pool));                                           \
+	}                                                                                              \
+	SYS_INIT(_name##_init, APPLICATION, CONFIG_APPLICATION_INIT_PRIORITY);
 
 #endif /* ZSLICE_INCLUDE_ZSLICE_H_ */
---
+++ src/zslice.c
@@ -0,0 +1,16 @@
+#include <zephyr/kernel.h>
+#include <zephyr/net_buf.h>
+#include <zephyr/data/cobs.h>
+
+#include <pb_encode.h>
+#include <pb_decode.h>
+
+#include <zephyr/logging/log.h>
+
+static int zslice_encode() {
+	
+}
+
+static int zslice_decode() {
+
+}
